a595bdc35a06ff7b580a70574128f9ddd6d9d7b1
Dipaksha <88131735+Dipaksha@users.noreply.github.com>
2021-08-20 11:31:03 +0530

    update code

---
+++ Article-data.js
@@ -0,0 +1,84 @@
+import React, {useState,useEffect} from 'react';
+
+
+const buttonStyle={
+    color: "white",
+    padding: "15px 40px",
+    textAlign: "center",
+    textDecoration: "none",
+    fontSize: "16px",
+    margin: "40px 5px",
+    cursor: "pointer",
+    backgroundColor: "#6b6bb7",
+    borderRadius: "5%",
+    borderColor: "transparent"
+}
+function Article(){
+    const [articlesData, setArticlesData] = useState([]);
+    const BASE_URL = 'https://run.mocky.io/v3/99da4b92-4c70-4c5e-ac01-17dd4c1649d2';
+    const [isLoading, setIsLoading] = useState(false);
+    const [isDoing, setIsDoing] = useState(false);
+    
+    useEffect(() => {
+        console.log(articlesData);
+        fetchData();
+        
+    },[]);
+    
+    const fetchData = () => {
+        setIsLoading(true)
+        fetch((`${BASE_URL}`))
+            .then( (response) => response.json())
+
+            .then( (jsonData) => {
+                setIsLoading(false)
+                setArticlesData(jsonData.data)
+            })
+            
+            .catch((error)=>{
+                setIsLoading(false)
+            })
+    }
+
+    const onClickButton = () => {
+        setIsDoing(!isDoing);
+    }
+
+    return (
+        <div>
+            <>
+                <button style={buttonStyle} onClick={onClickButton}>Doing</button>
+                <button style={buttonStyle} onClick={onClickButton}>Waiting</button>
+            </>
+            {isDoing == true &&
+                articlesData.map((item,index) =>(
+                    item.status === 'doing' && 
+                    <>
+                        {isDoing === true && 
+                            <>
+                                <li>{item.title}</li>
+                                <li>{item.description}</li>
+                            </>
+                        }
+                    </>
+                ))
+            }
+            {isDoing == false &&
+                articlesData.map((item,index) =>(
+                    item.status === 'waiting' && 
+                    <>
+                        {isDoing === false && 
+                            <>
+                                <li>{item.title}</li>
+                                <li>{item.description}</li>
+                            </>
+                        }
+                    </>
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default Article;
